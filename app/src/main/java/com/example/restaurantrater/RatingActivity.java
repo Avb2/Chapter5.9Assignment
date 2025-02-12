8309621e19b6d8c7a06803a4ff600bd20bff46e2
Avb2 <bringuel.alexander@gmail.com>
2025-02-12 12:15:13 -0500

    dd

--- app/src/main/java/com/example/restaurantrater/RatingActivity.java
+++ app/src/main/java/com/example/restaurantrater/RatingActivity.java
@@ -1,6 +1,12 @@
 package com.example.restaurantrater;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +25,33 @@ public class RatingActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+
+
+    public void saveRatingButton(){
+        Button saveButton = findViewById(R.id.ratingSaveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText nameEditText = findViewById(R.id.nameEditText);
+                RatingBar ratingBar = findViewById(R.id.ratingBar);
+
+                /// Radio buttons
+                RadioButton entreeRadio = findViewById(R.id.entreeRadio);
+                RadioButton appetizerRadio = findViewById(R.id.appetizerRadio);
+                RadioButton dessertRadio = findViewById(R.id.dessertRadio);
+
+                if (entreeRadio.isChecked()) {
+
+                } else if (appetizerRadio.isChecked()) {
+
+                } else if (dessertRadio.isChecked()) {
+
+                }
+
+            }
         });
     }
 }
---
+++ app/src/main/java/com/example/restaurantrater/helper/RatingData.java
@@ -0,0 +1,40 @@
+package com.example.restaurantrater.helper;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.restaurantrater.dbs.RatingDb;
+import com.example.restaurantrater.models.RatingModel;
+
+public class RatingData {
+    private SQLiteDatabase db;
+    private RatingDb ratingDb;
+
+    public RatingData(Context context){
+        this.ratingDb = new RatingDb(context);
+    }
+
+    public void open() throws SQLException {
+        this.db = this.ratingDb.getWritableDatabase();
+    }
+
+    public void close() throws SQLException {
+        this.db.close();
+    }
+
+
+    public void insertVals(RatingModel ratingModel){
+        ContentValues vals = new ContentValues();
+
+        vals.put("name", ratingModel.getName());
+        vals.put("type", ratingModel.getType());
+        vals.put("rating", ratingModel.getRating());
+
+        try {
+            this.db.insert("ratings", null, vals);
+        } catch (Exception e) {
+        }
+    }
+}
