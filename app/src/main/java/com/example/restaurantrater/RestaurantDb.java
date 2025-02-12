7aef0b179d18c028785788a5e247a33dd21d527e
Avb2 <bringuel.alexander@gmail.com>
2025-02-12 11:35:37 -0500

    restaurantdb class

--- app/src/main/java/com/example/restaurantrater/RestaurantDb.java
+++ app/src/main/java/com/example/restaurantrater/RestaurantDb.java
@@ -1,16 +1,34 @@
 package com.example.restaurantrater;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class RestaurantDb extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "restaurants.db";
+    public static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS restaurants (" +
+            "restaurantid integer primary key autoincrement, " +
+            "name text not null, " +
+            "city text not null, " +
+            "address text not null, " +
+            "state text not null, " +
+            "zipcode integer not null);";
+
+
+    public RestaurantDb(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        sqLiteDatabase.execSQL(CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS restaurants");
+        onCreate(sqLiteDatabase);
     }
 }
---
+++ app/src/main/java/com/example/restaurantrater/helper/RestaurantData.java
@@ -0,0 +1,4 @@
+package com.example.restaurantrater.helper;
+
+public class RestaurantData {
+}
